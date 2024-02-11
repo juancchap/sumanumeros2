f12db43158170350cd21b11c51ec17f99147a944
juan carlos <juan carlos@LAPTOP-O0H18VAV>
2024-02-11 08:41:35 -0600

    Tabla de multiplicacion

--- src/sumanumeros2/Sumanumeros2.java
+++ src/sumanumeros2/Sumanumeros2.java
@@ -1,23 +1,17 @@
-
 package sumanumeros2;
-
-
 
 public class Sumanumeros2 {
 
     public static void main(String[] args) {
-       int i;
-       int suma=0;
-       
-       for (i=1;i<= 12;i++){
-          suma+= i-1;
-          System.out.println(suma); 
-       }
+        int i;
+        int suma = 0;
 
-        
+        for (i = 1; i <= 12; i++)
+        {
+            suma += i - 1;
+            System.out.println(suma);
+        }
 
-
-        
     }
-    
+
 }
